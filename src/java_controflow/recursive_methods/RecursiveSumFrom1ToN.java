f2b648b85e848e9d35b914fba62f75df665570d4
Lahcen ELORF <lahcenelorf.24@gmail.com>
2025-01-30 19:01:36 +0100

    implement same recursive functions

--- src/java_controflow/recursive_methods/RecursiveSumFrom1ToN.java
+++ src/java_controflow/recursive_methods/RecursiveSumFrom1ToN.java
@@ -2,21 +2,21 @@ package java_controflow.recursive_methods;
 
 import java.util.Scanner;
 
-public class RecursiveFactorialCalculation {
-    public static long factorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        return n * factorial(n - 1);
-    }
+public class RecursiveSumFrom1ToN {
 
+    public static long sumFrom1ToN(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sumFrom1ToN(n - 1);
+    }
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         System.out.print("Enter a number : ");
+
         int num = input.nextInt();
 
-        System.out.println("\nFactorial of " + num + " is " + factorial(num));
+        System.out.println("Sum of " + num + " to 1 is : " + sumFrom1ToN(num));
     }
 }
