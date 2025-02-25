a791705752e499aee73d16166b19d44b72383468
Lahcen ELORF <lahcenelorf.24@gmail.com>
2025-02-25 19:25:50 +0000

    feat(basics/methods): Solve some problems (CheckLeapYear, PasswordValidator, ...)

---
+++ src/basics/methods/PasswordValidator.java
@@ -0,0 +1,10 @@
+package basics.methods;
+
+public class ValidatePassword {
+    public static boolean checkPassword(String password) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("\n################################ Validate Password ################################");
+    }
+}
